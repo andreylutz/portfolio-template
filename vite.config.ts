1c9ebc376af0ed41b60100036e00e7ee8acb2a7d
andreylutz Hobby GITHUB Profile <andreylutcenko123@gmail.com>
2026-05-01 21:46:21 +0300

    feat: add CICD

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isGithubPagesBuild = process.env.GITHUB_ACTIONS === "true" && Boolean(repoName);
+
 export default defineConfig({
+  base: isGithubPagesBuild ? `/${repoName}/` : "/",
   plugins: [react()]
 });
