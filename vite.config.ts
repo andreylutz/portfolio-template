f66db3eeaee1a148e80c48d7d380dbd00d4e4810
andreylutz Hobby GITHUB Profile <andreylutcenko123@gmail.com>
2026-05-01 22:01:20 +0300

    feat: base tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const isGithubPagesBuild = process.env.GITHUB_ACTIONS === "true" && Boolean(repoName);
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  base: isGithubPagesBuild ? `/${repoName}/` : "/",
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.ts"
+  }
 });
